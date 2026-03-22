ef5bc4551b737351926fe0cf463f8e36ddae0c1c
Poojani <llokuliyana@student.unimelb.edu.au>
2026-03-22 20:39:02 +1100

    add player model with movement, buying and rent logic

--- game/player.py
+++ game/player.py
@@ -1,4 +1,6 @@
 class Player:
+    """Represents a player in the game"""
+
     def __init__(self, name, starting_balance=16):
         self.name = name
         self.balance = starting_balance
@@ -6,17 +8,22 @@ class Player:
         self.properties = []
 
     def move(self, steps, board_size):
+        """Move the player by a number of steps, returning True if they passed GO."""
         old_position = self.position
         self.position = (self.position + steps) % board_size
-        passed_go = self.position < old_position or (old_position + steps) >= board_size
+
+        # Passed GO if the new position is less than the old or if steps carried past the end
+        passed_go = (old_position + steps) >= board_size
         return passed_go
 
     def buy(self, property):
+        """Buy a property, deducting the price from the player's balance"""
         self.balance -= property.price
         property.owner = self
         self.properties.append(property)
 
     def pay_rent(self, amount, owner):
+        """Pay rent to another player"""
         self.balance -= amount
         owner.balance += amount
 
