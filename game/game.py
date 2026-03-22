dbb07ef7a572e6b9fc3daef2acfa771dd92c9afa
Poojani <llokuliyana@student.unimelb.edu.au>
2026-03-22 20:43:35 +1100

    Add game class with turn logic and rent calculation

--- game/game.py
+++ game/game.py
@@ -1,10 +1,12 @@
 from game.board import Board, Property
 from game.player import Player
+from game.utils import load_json
 
 
 class Game:
     def __init__(self, board_path="board.json"):
         self.board = Board(board_path)
+        # Players take turns in this order
         self.players = [
             Player("Peter"),
             Player("Billy"),
@@ -12,20 +14,41 @@ class Game:
             Player("Sweedal"),
         ]
 
-    def load_rolls(self, rolls_path):
-        import json
-        with open(rolls_path) as f:
-            return json.load(f)
-
     def get_rent(self, property):
         colour_properties = self.board.get_properties_by_colour(property.colour)
         all_owned_by_same = all(p.owner == property.owner for p in colour_properties)
-        if all_owned_by_same:
-            return property.price * 2
-        return property.price
+        return property.rent(all_owned_by_same)
+
+    def take_turn(self, player, roll):
+        passed_go = player.move(roll, self.board.size())
+
+        # Award $1 for passing GO
+        if passed_go:
+            player.balance += 1
+            print(f"{player.name} passed GO, collected $1")
+
+        space = self.board.get_space(player.position)
+        print(f"{player.name} rolled {roll} and landed on {space.name}")
+
+        if isinstance(space, Property):
+            if space.owner is None:
+                # Property is unowned, player must buy it
+                player.buy(space)
+                print(f"{player.name} bought {space.name} for ${space.price}")
+            elif space.owner != player:
+                # if property is owned by another player, pay rent
+                rent = self.get_rent(space)
+                player.pay_rent(rent, space.owner)
+                print(f"{player.name} paid ${rent} rent to {space.owner.name}")
+
+    def is_game_over(self):
+        for player in self.players:
+            if player.is_bankrupt():
+                return player
+        return None
 
     def play(self, rolls_path):
-        rolls = self.load_rolls(rolls_path)
+        rolls = load_json(rolls_path)
         roll_index = 0
 
         while roll_index < len(rolls):
@@ -36,27 +59,14 @@ class Game:
                 roll = rolls[roll_index]
                 roll_index += 1
 
-                passed_go = player.move(roll, self.board.size())
-                if passed_go:
-                    player.balance += 1
-                    print(f"{player.name} passed GO, collected $1")
+                self.take_turn(player, roll)
 
-                space = self.board.get_space(player.position)
-                print(f"{player.name} rolled {roll} and landed on {space.name}")
-
-                if isinstance(space, Property):
-                    if space.owner is None:
-                        player.buy(space)
-                        print(f"{player.name} bought {space.name} for ${space.price}")
-                    elif space.owner != player:
-                        rent = self.get_rent(space)
-                        player.pay_rent(rent, space.owner)
-                        print(f"{player.name} paid ${rent} rent to {space.owner.name}")
-
-                        if player.is_bankrupt():
-                            print(f"\n{player.name} is bankrupt!")
-                            self.print_results()
-                            return
+                # Check for bankrupt players after each turn
+                bankrupt_player = self.is_game_over()
+                if bankrupt_player:
+                    print(f"\n{bankrupt_player.name} is bankrupt!")
+                    self.print_results()
+                    return
 
         self.print_results()
 
