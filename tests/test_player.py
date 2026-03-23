6398df5214a5962fc6f278b468e8e1cd9384c093
Poojani <llokuliyana@student.unimelb.edu.au>
2026-03-23 21:42:55 +1100

    Added comments to test_player

--- tests/test_player.py
+++ tests/test_player.py
@@ -6,6 +6,7 @@ from game.board import Property
 class TestPlayer(unittest.TestCase):
 
     def setUp(self):
+        # Create fresh players before each test
         self.player = Player("Peter")
         self.other = Player("Billy")
 
@@ -16,6 +17,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player.position, 0)
 
     def test_moves_correctly(self):
+        # Player should move to the new position after a roll
         self.player.move(3, 9)
         self.assertEqual(self.player.position, 3)
 
@@ -25,17 +27,20 @@ class TestPlayer(unittest.TestCase):
         self.assertTrue(passed_go)
 
     def test_buys_property(self):
+        # Buying a property deducts money and set ownership
         prop = Property({"name": "The Burvale", "type": "property", "price": 1, "colour": "Brown"})
         self.player.buy(prop)
         self.assertEqual(self.player.balance, 15)
         self.assertEqual(prop.owner, self.player)
 
     def test_pays_rent(self):
+        # Paying rent should transfer money from payer to the owner
         self.player.pay_rent(3, self.other)
         self.assertEqual(self.player.balance, 13)
         self.assertEqual(self.other.balance, 19)
 
     def test_goes_bankrupt(self):
+        # Negative balance means the player is bankrupt
         self.player.balance = -1
         self.assertTrue(self.player.is_bankrupt())
 
